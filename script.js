595ac776359392b8e58108d735197c509a6c1c73
crisLeo-P01 <cristianleopaiva@gmail.com>
2023-01-09 15:20:20 -0300

    Dando funcionalidad a los elementos

--- script.js
+++ script.js
@@ -5,42 +5,60 @@ const inputDias = document.querySelector('#diasInversion');
 const btnEnviar = document.querySelector('#btnEnviar');
 const btnBorrar = document.querySelector('#btnBorrar');
 
+// TABLA DE RESULTADOS
+const plazoDias = document.querySelector('#plazo-dias');
+const capital = document.querySelector('#capital');
+const interesesGanados = document.querySelector('#intereses-ganados');
+const montoTotal = document.querySelector('#monto-total');
+const tablaResultado = document.querySelector('#contain-datos-inversion');
+
 const formInValid = {
   inputIngresos: false,
   inputDias: false
 };
 
 inputIngresos.addEventListener('change', (e) => {
- if( e.target.value < 500 ) {
-   cartelWarning.classList.remove('d-none');
-   cartelWarning.textContent = 'Tu inversion debe superar los $500(dólares)'
-  } else if( e.target.value > 10000 ) {
+  const inputDatosIngresos = e.target.value;
+  console.log(inputDatosIngresos);
+  if( inputDatosIngresos < 500 ) {
+    cartelWarning.classList.remove('d-none');
+    cartelWarning.textContent = 'Tu inversion debe superar los $500(dólares)'
+  } else if( inputDatosIngresos > 10000 ) {
     cartelWarning.classList.remove('d-none');
     cartelWarning.textContent = 'Tu inversion debe no debe superar los $10.000(dólares)'
   } else {
-   cartelWarning.classList.add('d-none');
- }
+    cartelWarning.classList.add('d-none');
+    capital.textContent = 'Capital: $' + inputDatosIngresos;
+  }
 })
 
+
 inputDias.addEventListener('change', (e) => {
-  if( e.target.value < 30 ) {
+  const inputDatosDias = e.target.value;
+  console.log(inputDatosDias);
+  if( inputDatosDias < 30 ) {
     cartelWarning.classList.remove('d-none');
     cartelWarning.textContent = 'La cantidad de días mínima debe ser de 30 días'
-  } else if( e.target.value > 365 ) {
+  } else if( inputDatosDias > 365 ) {
     cartelWarning.classList.remove('d-none');
     cartelWarning.textContent = 'La cantidad de días máxima debe ser de 365 días'
   } else {
     cartelWarning.classList.add('d-none');
+    plazoDias.textContent = 'Plazo: ' + inputDatosDias + ' días.';
   }
 })
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  tablaResultado.classList.remove('d-none');
 })
 
 btnBorrar.addEventListener('click', () => {
   form.reset();
   cartelWarning.classList.add('d-none');
+  tablaResultado.classList.add('d-none');
 })
+
+
 
 
